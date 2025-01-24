b90969261111e6e635d854996bf55ad3e82658f5
sudhir18sn <sudhirnishad517@gmail.com>
2025-01-24 21:18:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -96,7 +96,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sudhir18sn/SUDHIR-MUSIC",
+    "https://github.com/sudhir18sn/cyber",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
